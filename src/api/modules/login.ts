ec04154791ffff84b73df9a720b7f34df39a9bb9
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-09-11 12:07:58 -0300

    remove módulo inútil

--- src/api/modules/login.ts
+++
@@ -1,9 +0,0 @@
-import service from '@/api/http';
-
-const serviceApi = {
-  // 验证登录实例
-  postVerification: (params: object) => service.post(`/login`, params),
-  getData: (params: string) => service.get(`/${params}`),
-};
-
-export default serviceApi;
