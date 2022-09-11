ebee61484fa70ce20f78d95af2728673969b7d95
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-09-11 11:50:02 -0300

    active link menu update

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,11 +1,5 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    :ellipsis="false"
-    @select="handleSelect"
-  >
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false">
     <el-menu-item index="0">
       <router-link class="icon-btn mx-2" to="/">
         <span>HOME</span>
@@ -22,28 +16,28 @@
         <span>JOKES </span>
       </router-link>
     </el-menu-item>
-    <!-- <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu> -->
   </el-menu>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const activeIndex = ref('1');
-const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
+const route = useRoute();
+const activeIndex = ref(route.path);
+watch(
+  route,
+  (newValue) => {
+    if (newValue) {
+      const names = newValue.name;
+      activeIndex.value = ['home', 'poke', 'jokes']
+        .findIndex((name: string) => {
+          return name === names;
+        })
+        .toString();
+    }
+  },
+  { flush: 'pre', immediate: true, deep: true },
+);
 </script>
 
 <style>
